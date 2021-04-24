26782e9e75705595170a97d976e05af64db517d0
cndesabbata <cndesabbata@outlook.com>
2021-04-24 11:59:10 +0200

    Add files via upload

---
+++ TIW_Playlist/WebContent/src/it/polimi/tiw/projects/controllers/GoToHomepage.java
@@ -0,0 +1,88 @@
+package it.polimi.tiw.projects.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.projects.beans.Playlist;
+import it.polimi.tiw.projects.beans.User;
+import it.polimi.tiw.projects.dao.PlaylistDAO;
+import it.polimi.tiw.projects.utils.ConnectionHandler;
+
+@WebServlet("/Homepage")
+public class GoToHomepage extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	private Connection connection = null;
+
+	public GoToHomepage() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// If the user is not logged in, redirects to login page
+		String loginPath = getServletContext().getContextPath() + "/index.html";
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			response.sendRedirect(loginPath);
+			return;
+		}
+		// Create list of the user playlists
+		User user = (User) session.getAttribute("user");
+		PlaylistDAO playlistDAO = new PlaylistDAO(connection);
+		List<Playlist> playlists = new ArrayList<Playlist>();
+		try {
+			playlists = playlistsDAO.findPlaylistByUser(user.getId());
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to recover playlists");
+			return;
+		}
+
+		// Redirect to the Home page and add missions to the parameters
+		String path = "/WEB-INF/Homepage.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		ctx.setVariable("playlists", playlists);
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
